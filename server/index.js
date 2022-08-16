90ffa28ed1870df78320e1f76f7678b03d399d9e
blakeahalt <blakeahalt@gmail.com>
2022-08-16 00:28:38 -0700

    deploy

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use(express.static(path.resolve(__dirname, '../client/build')));
+app.use(express.static(path.resolve(__dirname, '/public')));
 
 
 app.get("/api", (req, res) => {
