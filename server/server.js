a1aa202f4b69a2c0c24fa79676866c2de98d9c49
blakeahalt <blakeahalt@gmail.com>
2022-08-22 23:57:44 -0400

    build->public

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ const app = express();
 const port = process.env.PORT || 3001;
 
 
-app.use(express.static(path.join(__dirname, "build")));
+app.use(express.static(path.join(__dirname, "./public/index.html")));
 
 // if (process.env.NODE_ENV === "production") {
 //   app.use(express.static(path.join(__dirname, "/client/build")));
