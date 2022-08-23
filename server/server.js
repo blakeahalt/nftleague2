f47180967afd001133f154a39c1d3dee35fd6f19
blakeahalt <blakeahalt@gmail.com>
2022-08-23 00:21:44 -0400

    express__dirname

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ const cors = require("cors");
 app.use(cors());
 app.use(express.json());
 
-app.use(express.static(path.join(__dirname, "client", "build")));
+app.use(express.static(path.join(__dirname, "public")));
 // app.use(express.static(path.resolve(__dirname, '/public')));
 
 app.get("/api", (req, res) => {
