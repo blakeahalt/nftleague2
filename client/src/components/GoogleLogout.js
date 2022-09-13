8677cee2de0767055eb53426ade59882db14a8c0
blakeahalt <blakeahalt@gmail.com>
2022-09-13 13:42:35 -0700

    reset <Link.../Link>

--- client/src/components/GoogleLogout.js
+++ client/src/components/GoogleLogout.js
@@ -1,4 +1,6 @@
 import { GoogleLogout } from 'react-google-login'
+import { Link } from 'react-router-dom';
+
 
 const clientId="1077671935526-r9547hfdu1l45omb8s10jjehbv309rki.apps.googleusercontent.com"
 
@@ -7,14 +9,16 @@ function GLogout() {
 	const onSuccess = () => {
 		console.log("Log Out successful!");
 	}
-
+	
 	return (
 		<div>
+			<Link to="/register">
 			<GoogleLogout
 				clientId={clientId}
 				buttonText={"Logout"}
 				onLogoutSuccess={onSuccess}
-			/>
+				/>
+			</Link>
 		</div>
 	)
 }
