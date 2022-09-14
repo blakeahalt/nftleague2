2905f85ff093db18002fdbc23f8fe45df8ed0ab1
blakeahalt <blakeahalt@gmail.com>
2022-09-13 21:03:41 -0700

    Heroku server.js fix

--- server/server.js
+++ server/server.js
@@ -9,12 +9,12 @@ app.use(cors());
 app.use(express.json());
 const mysql = require('mysql')
 
-const {encrypt, decrypt} = require('./EncryptionHandler')
+const { encrypt, decrypt } = require('./EncryptionHandler')
 
 const db = mysql.createConnection({
-  user: 'root' ,
+  user: 'root',
   host: 'localhost',
-  password:'Pass*3827',
+  password: 'Pass*3827',
   database: 'passwordManager'
 })
 
@@ -26,27 +26,27 @@ app.get("/api", (req, res) => {
 });
 
 app.get("/test", (req, res) => {
-  res.json({ message:"WORKING" });
+  res.json({ message: "WORKING" });
 });
 
 app.post("/test", (req, res) => {
-  res.json({ message:"WORKING" });
+  res.json({ message: "WORKING" });
 });
 
 app.get("/GoogleApp", (req, res) => {
-  res.json({ message:"WORKING" });
+  res.json({ message: "WORKING" });
 });
 
 app.get("/notification", (req, res) => {
-  res.json({ message:"WORKING" });
+  res.json({ message: "WORKING" });
 });
 
 app.get("/register", (req, res) => {
-  res.json({ message:"WORKING" });
+  res.json({ message: "WORKING" });
 });
 
 app.get("/profile", (req, res) => {
-  res.json({ message:"WORKING" });
+  res.json({ message: "WORKING" });
 });
 
 app.get("/added", (req, res) => {
@@ -71,29 +71,29 @@ app.post("/register", (req, res) => {
 });
 
 app.post("/addPassword", (req, res) => {
-  const {pwd, user} = req.body 
+  const { pwd, user } = req.body
   const hashedPassword = encrypt(pwd)
 
   db.query("INSERT INTO passwords (password, user, iv) VALUES (?,?,?)", [hashedPassword.password, user, hashedPassword.iv],
-  (err, result) => {
-    if (err) {
-      console.log(err);
-    } else {
-      res.send("Success")
-    }
-  })
+    (err, result) => {
+      if (err) {
+        console.log(err);
+      } else {
+        res.send("Success")
+      }
+    })
 });
 
-  
+
 app.get("/showPasswords", (req, res) => {
-  db.query("SELECT * FROM passwords;", 
-  (err, result) => {
-    if(err) {
-      console.log(err);
-    } else {
-      res.send(result)
-    }
-  })
+  db.query("SELECT * FROM passwords;",
+    (err, result) => {
+      if (err) {
+        console.log(err);
+      } else {
+        res.send(result)
+      }
+    })
 })
 
 app.post("/decryptpassword", (req, res) => {
@@ -107,7 +107,7 @@ app.post("/decryptpassword", (req, res) => {
 
 // if (process.env.NODE_ENV === "production") {
 //   app.use(express.static(path.join(__dirname, "/client/build")));
-  
+
 
 // This route serves the React app
 app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, "public", "index.html")));
