a982853cff7d4a132872f0f83ab7e35110ac6f05
blakeahalt <blakeahalt@gmail.com>
2022-10-04 21:54:41 -0700

    runbuild8

--- server/server.js
+++ server/server.js
@@ -105,7 +105,7 @@ app.use(function(req, res, next) {
 });
 
 // app.use(express.static(path.join(__dirname, "public")));
-app.use(express.static(path.join(__dirname, '/../client/public')));
+app.use(express.static(path.join(__dirname, '../client/public')));
 
 
 // app.use(express.static(path.join(__dirname + "/client/build")));
