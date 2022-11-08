b7269036934c6fc32689639f504db000da73828f
blakeahalt <blakeahalt@gmail.com>
2022-11-08 01:28:17 -0800

    run build18

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-const port = process.env.PORT || 3001;
+const port = process.env.NODE_ENV || 3001;
 const cors = require('cors');
 const argon2 = require('argon2');
 // const crypto = require('crypto');
